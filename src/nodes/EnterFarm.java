258957ace4692622180b1467e0465106e1e01944
David-Rodden <davidrodden273@gmail.com>
2021-10-30 01:45:57 -0700

    Farm tile entrance added

--- src/nodes/EnterFarm.java
+++ src/nodes/EnterFarm.java
@@ -1,8 +1,10 @@
 package nodes;
 
 import org.powbot.api.Condition;
+import org.powbot.api.Tile;
 import org.powbot.api.rt4.Chat;
 import org.powbot.api.rt4.ChatOption;
+import org.powbot.api.rt4.Movement;
 import org.powbot.api.rt4.Objects;
 import structure.TreeTask;
 
@@ -10,14 +12,22 @@ import structure.TreeTask;
  * Enter the farm when player has seeds to farm
  */
 public class EnterFarm extends TreeTask {
+    private final Tile farmEntrance;
+
     public EnterFarm() {
         super(true);
+        farmEntrance = new Tile(1804, 3501);
     }
 
     @Override
     public int execute() {
+        if (farmEntrance.distance() > 3) {
+            Movement.walkTo(farmEntrance);
+            Condition.wait(() -> farmEntrance.distance() <= 3, 1500, 3);
+            return super.execute();
+        }
         if (!Chat.chatting()) Objects.stream().within(15).name("Farm door").action("Open").findAny().ifPresent(d -> {
-            if(!d.interact("Open")) return;
+            if (!d.interact("Open")) return;
             Condition.wait(() -> Objects.stream().within(10).name("Sack").action("Deposit").reachable().isNotEmpty() || Chat.chatting(), 2000, 4);
         });
         if (Chat.canContinue()) {
--- src/nodes/ShouldPlant.java
+++ src/nodes/ShouldPlant.java
@@ -1,18 +1,23 @@
 package nodes;
 
+import org.powbot.api.rt4.Inventory;
 import structure.PositionRecord;
 import structure.TreeTask;
 
+import java.util.regex.Pattern;
+
 public class ShouldPlant extends TreeTask {
     private final PositionRecord record;
+    private final Pattern seedling;
 
     public ShouldPlant(final PositionRecord record) {
         super(false);
         this.record = record;
+        seedling = PlantInfo.SEEDLING.getPattern();
     }
 
     @Override
     public boolean validate() {
-        return record.size() < 16;
+        return record.size() < 16 && Inventory.stream().name(seedling).isNotEmpty();
     }
 }
